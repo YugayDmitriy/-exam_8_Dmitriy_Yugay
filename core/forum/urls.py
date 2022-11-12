12f399ac232dc2559f7a1d3f1088caaa93c4a6a7
Dmitriy <Dmitrijugaj614@gmail.com>
2022-11-12 18:50:42 +0600

    Добавлены пути приложения

--- core/forum/urls.py
+++ core/forum/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from forum.views import IndexView, board_topics, new_topic, topic_posts, reply_topic
 
-
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^boards/(?P<pk>\d+)/$', board_topics, name='board_topics'),
@@ -9,3 +8,4 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', reply_topic, name='reply_topic'),
 ]
+
